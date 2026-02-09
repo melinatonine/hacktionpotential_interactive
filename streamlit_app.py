24303e82528cfc6b0843baa956a5cd46b6220938
Mélina SCOPIN <94561153+melinatonine@users.noreply.github.com>
2026-02-09 18:42:15 +0000

    google_sheet_first_test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Interactive session - homepage")
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = pd.DataFrame({'coucou' : [1,2], 'hey' : [3,4]})
+
+# click button to update worksheet
+# This is behind a button to avoid exceeding Google API Quota
+if st.button("Update worksheet"):
+    df = conn.update(data=df)
+    st.cache_data.clear()
+    st.rerun()
+
+
+
