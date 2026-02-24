5ea6fa54bc15cf3b953e6cdcea3016289d43678d
melinatonine <melinaascopin@gmail.com>
2026-02-24 19:04:48 +0100

    clean states

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,155 +3,165 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import time
 from streamlit_autorefresh import st_autorefresh
+from random import randint 
 
 st.title("Interactive session - homepage")
 
-
-login, word_game, sound_game, attention, stroop, time_aware = st.tabs(["Identification", "GAME 1", "GAME 2", "GAME 3", "GAME 4", "GAME 5"])
+tabs = ["Registration", "GAME 1", "GAME 2", "GAME 3", "GAME 4", "GAME 5"]
+login, word_game, sound_game, attention, stroop, time_aware = st.tabs(tabs)
 
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
+if 'user' not in st.session_state:
+    st.session_state.user = randint(0,1000)
+    st.session_state.tab_step = [0 for _ in range (len(tabs))]
+    st.session_state.trial =  0 
+
+
+
+def initialisation_game(dict_state) : 
+    for key in dict_state.keys() : 
+        st.session_state[key] = dict_state[key] 
+                        
+@st.cache_data
+def read_sheet(sheet) : 
+    return conn.read(worksheet = sheet)
+
+@st.cache_data
+def write_sheet(sheet, df) : 
+    return conn.update(data=df, worksheet = sheet) 
+
+
+def save_click(game, label, position) : 
+    if st.session_state[f"type_click_{game}"][position] is None : 
+        st.session_state[f"type_click_{game}"][position] = label
+        st.session_state[f"delay_{game}"][position] = time.time()
+
+def restart(game, button = 'Restart?') : 
+
+    if st.button(button, key = f'{game}_{st.session_state.trial}') : 
+        st.session_state.tab_step[game] = 0 
+        st.session_state.trial += 1
+        st.rerun()
+
+
+def over(title = 'this part') : 
+    st.write(f'You are done with {title} {st.session_state.user}')
+
+
 with login : 
 
-    if 'submit0' not in st.session_state:
-        st.session_state.submit0 = False
+    if st.session_state.tab_step[0] == 0 : 
 
-    if not st.session_state.submit0 : 
         username = st.text_input('username', placeholder = 'firstname.surname')
 
         if st.button('SUBMIT', key = 'submitted0') : 
-            user_df = conn.read(worksheet = 'users')
+            user_df = read_sheet('users')
             list_users = list(user_df['user-list'])
             if username in list_users : 
                 st.write('name already taken')
             else : 
                 user_df.loc[len(user_df.index)] = [username]
-                st.write(user_df)
-                user_df = conn.update(data=user_df, worksheet = 'users') 
-                st.session_state.submit0 = True
+                user_df = write_sheet('users', user_df)
                 st.session_state.user = username
+                st.session_state.tab_step[0] = 1
                 st.rerun()
-    
-    if st.session_state.submit0 : 
-        st.write('You are done with registration')
 
+    else : 
+        over('registration')
+        restart(0, "Change username?")
+        
 
 # GAME 1
 with word_game : 
 
-    if "words" not in st.session_state:
-        st.session_state.words = []
-
-    if "rerun" not in st.session_state:
-        st.session_state.rerun = False
-
-    if 'submit1' not in st.session_state:
-        st.session_state.submit1 = False
-
-
-    def listwords():
+    @st.cache_data
+    def listwords(words):
         word_list = ''
-        for word in st.session_state.words : 
+        for word in words : 
             word_list += f' {word}'
 
         st.write(word_list)
+    
+    if st.session_state.tab_step[1] == 0 : 
 
-    if not st.session_state.submit1 : 
-        if st.session_state.rerun == True:
-            st.session_state.rerun = False
-            st.rerun()
+        initialisation_game({'words' : []})
 
-        else:
-            w = st.text_input('Enter words here', value="", placeholder = 'Enter a word')
-            if st.button('Add word'):
+        with st.form('Words entry', clear_on_submit = True) : 
+            w = st.text_input('Enter words here', value="", placeholder = 'Enter a word', key='user_word')
+
+            if st.form_submit_button('Add word to list'):
                 if w != "":
-                    st.session_state.words.append(w)       
+                    st.session_state.words.append(w)    
+                    # st.session_state.user_word = ''  
 
-        listwords()
+            listwords(st.session_state.words)
 
-        if st.button('SUBMIT', key = 'submitted1') : 
-            st.session_state.submit1 = True
-            word_df = conn.read(worksheet = 'game1')
+        if st.button('Send word list', key = 'submitted1') : 
+            word_df = read_sheet('game1')
             for word in st.session_state.words : 
                 i = list(word_df.index[word_df.WORD == word])
                 if len(i) == 1 : 
                     word_df.loc[i[0], 'COUNT'] += 1
-            word_df = conn.update(data=word_df, worksheet = 'game1') 
+            word_df = write_sheet('game1', word_df)
+            st.session_state.tab_step[1] = 1
             st.rerun()
     
-    if st.session_state.submit1 : 
-        st.write('You are done with game 1')
+    else : 
+        over("game 1")
 
 
 with sound_game : 
 
+    if st.session_state.tab_step[2] == 0 :  
+
+        with st.form("Estimation of Sound Amplitudes") : 
+
+            amplitudes = [0 for _ in range (10)]
+            for s in range (10) : 
+                amp = st.slider(f'Sound {s+1}', min_value = 1, max_value = 10, key = f'amp_{s}')
+                amplitudes[s] = amp
+
+            if st.form_submit_button('SUBMIT', key = 'submitted2') : 
+                
+                sound_df = read_sheet('game2')
+                sound_df[st.session_state.user] = amplitudes         
+                sound_df = write_sheet('game2', sound_df)
+                st.session_state.tab_step[2] = 1
+                st.rerun()
     
-    if 'submit2' not in st.session_state:
-        st.session_state.submit2 = False
-
-
-    if not st.session_state.submit2 : 
-        amplitudes = [0 for _ in range (10)]
-        for s in range (10) : 
-            amp = st.slider(f'Sound {s+1}', min_value = 1, max_value = 10, key = f'amp_{s}')
-            amplitudes[s] = amp
-
-
-        if st.button('SUBMIT', key = 'submitted2') : 
-            st.session_state.submit2 = True
-            sound_df = conn.read(worksheet = 'game2')
-            sound_df[st.session_state.user] = amplitudes         
-            sound_df = conn.update(data=sound_df, worksheet = 'game2')
-            st.rerun()
-    
-    if st.session_state.submit2 : 
-        st.write('You are done with game 2')
+    else : 
+        over('game 2')
+        
+        restart(2)
 
 with attention : 
 
     dt = 2
-    
-    
-    if "letter_count" not in st.session_state:
-        st.session_state.letter_count = 0
 
-    if 'started' not in st.session_state:
-        st.session_state.started = False
+    if st.session_state.tab_step[3] == 0 : 
 
-    if 'finished' not in st.session_state:
-        st.session_state.finished = False
-
-
-    def x_click() : 
-        st.session_state.x_clicks[st.session_state.letter_index] = time.time()
-    
-    def not_x_click() : 
-        st.session_state.not_x_clicks[st.session_state.letter_index] = time.time()
-
-    if not st.session_state.started and not st.session_state.finished:
+        initialisation_game({"letter_count": 0})
 
         if st.button('initialisation', key = 'init3') : 
-            attention_df = conn.read(worksheet = 'game3s')
-            st.session_state.letters = attention_df['LETTER']
-
-        if st.button("Start", key = 'st3'):
-            st.session_state.started = True
-            st.session_state.start_time = time.time()
-            st.session_state.letter_index = 0
             
-            letters = st.session_state.letters
-
-            if "x_clicks" not in st.session_state:
-                st.session_state.x_clicks = [None for _ in range (len(letters))]
-            
-            if "not_x_clicks" not in st.session_state:
-                st.session_state.not_x_clicks = [None for _ in range (len(letters))]
-                
+            attention_df = read_sheet('game3s')
+            letters = attention_df['LETTER']
+            initialisation_game({"letters" : letters, "type_click_3" : [None for _ in range (len(letters))], "delay_3" : [None for _ in range (len(letters))]})
+            st.session_state.tab_step[3] = 1 
             st.rerun()
 
-    if st.session_state.started and not st.session_state.finished :
+    elif st.session_state.tab_step[3] == 1 : 
+        
+        if st.button("Start", key = 'st3'):
+            initialisation_game({"start_time" : time.time(), "letter_index" : 0})   
+            st.session_state.tab_step[3] = 2 
+            st.rerun()
+    
+    elif st.session_state.tab_step[3] == 2 :
+
         letters = st.session_state.letters
         count = st_autorefresh(interval=100, limit=int(10*dt*len(letters)+5), key="refresher")
         elapsed = time.time() - st.session_state.start_time
@@ -160,101 +170,60 @@ with attention :
         if st.session_state.letter_index < len(letters):
             letter = letters[st.session_state.letter_index]
             st.write(letter)             
-
-            st.button("Click if X", on_click = x_click, key = 'clickx')
-            st.button("Click if not X", on_click = not_x_click, key = 'clicknotx')
+            st.button("Click if X", on_click = save_click, args = [3, "X", st.session_state.letter_index], key = 'clickx')
+            st.button("Click if not X", on_click = save_click, args = [3, "not_X", st.session_state.letter_index],  key = 'clicknotx')
 
         else:
-            success = [None for _ in range (len(letters))]
-            rts = [None for _ in range (len(letters))]
-
-            times_letters = [st.session_state.start_time+i*dt for i in range (len(letters))]
-
-            for k in range (len(letters)) : 
-                xt = st.session_state.x_clicks[k]
-                not_xt = st.session_state.not_x_clicks[k]
-                lt = times_letters[k]
-    
-                if  xt != None and not_xt == None : 
-                    rts[k] = xt - lt
-                    if letters[k] == 'X' : 
-                        success[k] = 1
-                    else : 
-                        success[k] = 0
-
-                elif xt == None and not_xt != None : 
-                    rts[k] = not_xt - lt
-                    if letters[k] != 'X' : 
-                        success[k] = 1
-                    else : 
-                        success[k] = 0
-
-            st.session_state.success = success
-            st.session_state.rts = rts
+            success = [1 if (letters[k] == "X" and st.session_state['type_click_3'] == "X") or (letters[k] != "X" and st.session_state['type_click_3'] == "not_X") else 0 for k in range (len(letters))]
+            rts = st.session_state['delay_3']
 
             if st.button('submit results') : 
-                attention_df = conn.read(worksheet = 'game3s')
+                attention_df = read_sheet('game3s')
                 attention_df[st.session_state.user] = success         
-                attention_df = conn.update(data=attention_df, worksheet = 'game3s')
+                attention_df = write_sheet('game3s', attention_df)
 
-                attention_df = conn.read(worksheet = 'game3t')
-                attention_df[st.session_state.user] = rts   
-                attention_df = conn.update(data=attention_df, worksheet = 'game3t') 
-            
-                st.session_state.finished = True
-            
+                attention_df = read_sheet('game3t')
+                attention_df[st.session_state.user] = rts         
+                attention_df = write_sheet('game3t', attention_df)
+
+                st.session_state.tab_step[3] = 3
                 st.rerun()
+        
+    else : 
+        over('game 3')
+        
+    restart(3)
 
-        
-    if st.session_state.finished : 
-        
-            
-        st.write('You are done with game 3')
-        # st.write(st.session_state.success)
-        # st.write(st.session_state.rts)
 
 
 with stroop : 
 
     dt = 2
     list_colors = ['red', 'green', 'blue', 'yellow']
-    
-    if 'started4' not in st.session_state:
-        st.session_state.started4 = False
 
-    if 'finished4' not in st.session_state:
-        st.session_state.finished4 = False
-
-
-    def color_click(c) : 
-        st.session_state.color_clicks[color][st.session_state.color_index] = time.time()
-    
-
-    if not st.session_state.started4 and not st.session_state.finished4:
-
+    if st.session_state.tab_step[4] == 0 :
         if st.button('initialisation', key = 'init4') : 
-            stroop_df = conn.read(worksheet = 'game4s')
-            st.session_state.names = stroop_df['NAME']
-            st.session_state.colors = stroop_df['COLOR']
+            stroop_df = read_sheet('game4s')
+            names = stroop_df['NAME']
+            initialisation_game({"names" : names, "colors" : stroop_df['COLOR'], "type_click_4" : [None for _ in range (len(names))], 
+                                    "delay_4" : [None for _ in range (len(names))]})
+            st.session_state.tab_step[4] = 1 
+            st.rerun()
+        
+    elif st.session_state.tab_step[4] == 1 : 
 
         if st.button("Start", key = 'st4'):
-            st.session_state.started4 = True
-            st.session_state.start_time4 = time.time()
-            st.session_state.color_index = 0
-            
-            names = st.session_state.names
-            colors = st.session_state.colors
 
-            st.session_state.color_clicks = {color : [None for _ in range (len(names))] for color in list_colors}
-            
+            initialisation_game({"start_time_color" : time.time(), "color_index" : 0})
+            st.session_state.tab_step[4] = 2 
             st.rerun()
 
-    if st.session_state.started4 and not st.session_state.finished4 :
+    elif st.session_state.tab_step[4] == 2 :
         names = st.session_state.names
         colors = st.session_state.colors
 
         count = st_autorefresh(interval=100, limit=int(10*dt*len(names)+5), key="refresher4")
-        elapsed = time.time() - st.session_state.start_time4
+        elapsed = time.time() - st.session_state.start_time_color
         st.session_state.color_index = int(elapsed // dt)
 
         if st.session_state.color_index < len(names):
@@ -263,105 +232,72 @@ with stroop :
             st.write(f':{color}[{word}]')             
 
             for color_option in list_colors : 
-                st.button(color_option, on_click = color_click, args = [color_option])
-
+                st.button(color_option, on_click = save_click, args = [4, color_option, st.session_state.color_index])
 
         else:
-            success = [0 for _ in range (len(names))]
-            rts = [None for _ in range (len(names))]
-
-            times_colors = [st.session_state.start_time4+i*dt for i in range (len(names))]
-            clicks = st.session_state.color_clicks
-
-            for k in range (len(names)) : 
-
-                color = names[k]
-
-                if clicks[color][k] != None : 
-                    success[k] = 1 
-                    rts[k] = clicks[color][k] - times_colors[k]
-                
-                else : 
-                    for c in list_colors : 
-                        if clicks[c][k] != None : 
-                            rts[k] = clicks[c][k] - times_colors[k]
-                
-
-            st.session_state.success4 = success
-            st.session_state.rts4 = rts
+            success = [1 if st.session_state['type_click_4'][k] == names[k] else 0 for k in range (len(names))]
+            rts = st.session_state['delay_4']
 
             if st.button('submit results') : 
-                stroop_df = conn.read(worksheet = 'game4s')
-                stroop_df[st.session_state.user] = success         
-                stroop_df = conn.update(data=stroop_df, worksheet = 'game4s')
+                color_df = read_sheet('game4s')
+                color_df[st.session_state.user] = success         
+                color_df = write_sheet('game4s', color_df)
 
-                stroop_df = conn.read(worksheet = 'game4t')
-                stroop_df[st.session_state.user] = rts   
-                stroop_df = conn.update(data=stroop_df, worksheet = 'game4t') 
-            
-                st.session_state.finished4 = True
-            
+                color_df = read_sheet('game4t')
+                color_df[st.session_state.user] = rts         
+                color_df = write_sheet('game4t', color_df)
+
+                st.session_state.tab_step[4] = 3
                 st.rerun()
+        
+    else : 
+        over('game 4')
+        
+    restart(4)
 
-        
-    if st.session_state.finished4 : 
-        
-            
-        st.write('You are done with game 4')
-        # st.write(st.session_state.success4)
-        # st.write(st.session_state.rts4)
+
 
 
 
 with time_aware : 
 
+    initialisation_game({"time_stop_click" : None})
+
     
-    if 'started5' not in st.session_state:
-        st.session_state.started5 = False
-
-    if 'finished5' not in st.session_state:
-        st.session_state.finished5 = False
-
-    if 'submitted5' not in st.session_state:
-        st.session_state.submitted5 = False
-
-
     def stop_click() : 
-        delta = time.time() - st.session_state.start_time5
-        st.session_state.stop_click = delta
-        st.session_state.finished5 = True
-    
+        delta = time.time() - st.session_state.start_time_guess
+        st.session_state.time_stop_click = delta
+        st.session_state.tab_step[5] = 2
 
-    if not st.session_state.started5 and not st.session_state.finished5 :
 
+    if st.session_state.tab_step[5] == 0  :
 
         if st.button("Start", key = 'st5'):
-            st.session_state.started5 = True
-            st.session_state.start_time5 = time.time()
-
-            st.session_state.stop_click = None
-            
+            st.session_state.start_time_guess = time.time()
+            st.session_state.tab_step[5] = 1
             st.rerun()
 
-    if st.session_state.started5 and not st.session_state.finished5 :
-        
+    elif st.session_state.tab_step[5] == 1 : 
         st.button('STOP', on_click = stop_click)
     
 
-    if st.session_state.finished5 and not st.session_state.submitted5 : 
+    elif st.session_state.tab_step[5] == 2: 
 
-        st.write(f'{int(st.session_state.stop_click)} seconds have passed')
+        st.write(f'{int(st.session_state.time_stop_click)} seconds have passed')
 
         if st.button('submit results') : 
-            aware_df = conn.read(worksheet = 'game5')
-            aware_df[st.session_state.user] = st.session_state.stop_click         
-            aware_df = conn.update(data=aware_df, worksheet = 'game5')
-            st.session_state.submitted5 = True
+            time_df = read_sheet('game5')
+            time_df[st.session_state.user] = st.session_state.time_stop_click         
+            time_df = write_sheet('game5', time_df)
+
+            st.session_state.tab_step[5] = 3
             st.rerun()
 
-    if st.session_state.submitted5 : 
-
-        st.write('You are done with game 5')
+    else : 
+        over('game 5')
+        
+    restart(5)
+   
 
             
             
